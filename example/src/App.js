5781f61dbc1e0318766c6e1b9b28e05a32440e86
Nir <nirberko@gmail.com>
2018-10-12 12:43:51 +0300

    first commit

---
+++ example/src/App.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react'
+import Measure from 'mezrmouse'
+
+import './app.css'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      distance: 0
+    }
+  }
+
+  render() {
+    const {distance} = this.state
+
+    return (
+      <div className='App'>
+        <div className='Form'>
+          <div className='Form__title'>
+            Create an account
+          </div>
+          <form>
+            <label>
+              <span>First Name *</span>
+              <input />
+              <div className='error' style={{opacity: distance}} />
+            </label>
+            <label>
+              <span>Last Name *</span>
+              <input value='Berko' />
+            </label>
+            <label>
+              <span>Email Address *</span>
+              <div className='error' style={{opacity: distance}} />
+              <input />
+            </label>
+            <label>
+              <span>Phone Number</span>
+              <input value='202-555-1243' />
+            </label>
+            <label style={{gridColumn: '2 span'}}>
+              <span>Company Name</span>
+              <input value='Electronics' />
+            </label>
+            <div className='actions'>
+              <Measure distance={100} onCloseUp={(percent) => this.setState({distance: percent})}>
+                <button disabled>Register</button>
+              </Measure>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default class Mezrmouse extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.node = React.createRef()
+
+    this.distance = props.distance
+
+    this.mousemoveEventListener = this.mousemoveEventListener.bind(this)
+  }
+
+  static propTypes = {
+    distance: React.PropTypes.number.isRequired,
+    onCloseUp: React.PropTypes.func,
+    children: React.PropTypes.node.isRequired
+  };
+
+  componentDidMount() {
+    document.addEventListener('mousemove', this.mousemoveEventListener)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousemove', this.mousemoveEventListener)
+  }
+
+  mousemoveEventListener(e) {
+    const {clientX, clientY} = e;
+
+    const {
+      x: nodeX,
+      y: nodeY,
+      width: nodeWidth,
+      height: nodeHeight
+    } = this.node.getBoundingClientRect();
+
+    const totalPercent = Math.round(Math.sqrt(Math.pow(nodeY + (nodeHeight / 2) - clientY, 2) + Math.pow(nodeX + (nodeWidth / 2) - clientX, 2))) / this.distance
+
+    if (totalPercent <= 1) {
+      this.props.onCloseUp((totalPercent - 1) * -1)
+    } else {
+      this.props.onCloseUp(0)
+    }
+  }
+
+  render() {
+    return React.cloneElement(this.props.children, {ref: (node) => { this.node = node }})
+  }
+}
